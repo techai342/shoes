98bb199744acdf6eb28a14a3cba9ce62bcefe03c
techai342 <tiktokusr4400@gmail.com>
2025-12-23 20:00:53 +0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -21,6 +20,8 @@ export default {
       animation: {
         'spin-slow': 'spin 12s linear infinite',
         'float-3d': 'float 6s ease-in-out infinite',
+        'bounce-slow': 'bounce 3s infinite',
+        'pulse-slow': 'pulse 4s cubic-bezier(0.4, 0, 0.6, 1) infinite',
       }
     },
   },
